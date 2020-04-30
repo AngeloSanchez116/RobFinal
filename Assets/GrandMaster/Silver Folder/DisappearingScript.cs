3d58b2280d56ab6b8c111c43c2c0ab253e9ccc36
AngeloSanchez116 <56123283+AngeloSanchez116@users.noreply.github.com>
2020-04-30 18:39:01 -0400

    Disappearing platform

---
+++ Assets/GrandMaster/Silver Folder/DisappearingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingScript : MonoBehaviour
+{
+    public GameObject disappearingPlatform;
+    public int disappearingTmer = 0;
+    public int appearingTmer = 0;
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && gameObject.tag == "Disappear")
+        {
+            yield return new WaitForSeconds(disappearingTmer);//wait x amount of seconds
+            disappearingPlatform.GetComponent<MeshRenderer>().enabled = false;
+            disappearingPlatform.GetComponent<BoxCollider>().enabled = false;
+            
+        }
+    }
+
+    IEnumerator OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && gameObject.tag == "Disappear")
+        {
+            yield return new WaitForSeconds(appearingTmer);//wait x amount of seconds
+            disappearingPlatform.GetComponent<MeshRenderer>().enabled = true;
+            disappearingPlatform.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
