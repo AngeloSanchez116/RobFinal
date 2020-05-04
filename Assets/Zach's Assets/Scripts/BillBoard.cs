500ce890691354b073709a29d75dc74094a0cc12
Zachary Kolibas <37418284+Wiggilis@users.noreply.github.com>
2020-05-03 23:55:57 -0400

    Level Build

---
+++ Assets/Zach's Assets/Scripts/BillBoard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BillBoard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    private void Start()
+    {
+        m_Camera = Camera.main;
+    }
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
